848e4cf170f92c776cc0656e2c0bdfbcd9dc8a0f
eleith <work@eleith.com>
2012-10-07 17:14:35 -0700

    allownull option

--- test/required.js
+++ test/required.js
@@ -17,4 +17,21 @@ describe("required schemas", function()
       expect(input1.valid).to.be.ok;
       expect(!input2.valid).to.be.ok;
    });
+
+   it("allownull", function()
+   {
+      var schema = schemajs.create(
+      {
+          input: {type:'string', required:true, allownull:true},
+          output:{type:'string'}
+      });
+
+      var input1 = schema.validate({input: 'username'});
+      var input2 = schema.validate({});
+      var input3 = schema.validate({input:null, output:"hi there"});
+  
+      expect(input1.valid).to.be.ok;
+      expect(!input2.valid).to.be.ok;
+      expect(input3.valid).to.be.ok;
+   });
 });
