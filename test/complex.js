8f0a3ff1e23e0a1ab5e37f023380c6ecc3c17a54
eleith <work@eleith.com>
2012-10-09 00:35:45 -0700

    allow null on complex items, tests to prove it

--- test/complex.js
+++ test/complex.js
@@ -17,16 +17,33 @@ describe("complex schemas", function()
             }
           }
       });
+
+      var schema2 = schemajs.create(
+      {
+          input: 
+          {
+            type:'array',
+            schema:
+            {
+               type: 'number'
+            },
+            allownull: true
+          }
+      });
       
       var input1 = schema.validate({input: 'username'});
       var input2 = schema.validate({input: [112390123]});
       var input3 = schema.validate({input: [112390123, 'username']});
       var input4 = schema.validate({});
-   
+      var input5 = schema.validate({input: null});
+      var input6 = schema2.validate({input: null});
+  
       expect(!input1.valid).to.be.ok;
       expect(input2.valid).to.be.ok;
       expect(!input3.valid).to.be.ok;
       expect(input4.valid).to.be.ok;
+      expect(!input5.valid).to.be.ok;
+      expect(input6.valid).to.be.ok;
    });
 
    it("arrays and objects", function()
@@ -76,15 +93,33 @@ describe("complex schemas", function()
             }
           }
       });
+
+      var schema2 = schemajs.create(
+      {
+          input: 
+          {
+            type:'object',
+            schema:
+            {
+               name:  { type: "string", properties: { max: 255 }, required: true},
+               email: { type: "email", error: "email is not a valid email address"}
+            },
+            allownull: true
+          }
+      });
       
       var input1 = schema.validate({input: 'username'});
       var input2 = schema.validate({input: {name:"x", email:"x@xyz.com"}});
       var input3 = schema.validate({input: {name:123, email:"x@xyz.com"}});
       var input4 = schema.validate({});
-   
+      var input5 = schema.validate({input:null});
+      var input6 = schema2.validate({input:null});
+
       expect(!input1.valid).to.be.ok;
       expect(input2.valid).to.be.ok;
       expect(input2.valid).to.be.ok;
       expect(input4.valid).to.be.ok;
+      expect(!input5.valid).to.be.ok;
+      expect(input6.valid).to.be.ok;
    });
 });
