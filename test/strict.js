9f802b164c78f3233cdd46a8a7958afbc6ff219b
orangemug <info@orangemug.co.uk>
2015-05-04 09:06:21 +0100

    Added additional test for adding strict via the schema object.

--- test/strict.js
+++ test/strict.js
@@ -4,13 +4,25 @@ describe("strict schemas", function()
    var schemajs   = (typeof window === 'undefined') ? require('../schema') : window.schema;
    var expect     = (typeof window === 'undefined') ? require('chai').expect : window.chai.expect;
 
-   it("additional keys strict", function()
+   it("additional keys, strict via argument", function()
    {
       var schema = schemajs.create({
         sound: {type:'string', 'default':'mooo'}
       });
 
       var input = schema.validate({sound: 'meow', type: "cat"}, {strict: true});
+      expect(input.valid).to.equal(false);
+      expect(input.errors.type).to.equal("Additional key not defined in schema");
+   });
+
+   it("additional keys, strict via schema object", function()
+   {
+      var schema = schemajs.create({
+        strict: true,
+        sound: {type:'string', 'default':'mooo'}
+      });
+
+      var input = schema.validate({sound: 'meow', type: "cat"});
       expect(input.valid).to.equal(false);
       expect(input.errors.type).to.equal("Additional key not defined in schema");
    });
